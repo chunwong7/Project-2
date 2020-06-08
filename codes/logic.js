abb62b8dab7871a46c7705221942dfe0f8e9ce3f
Randy Dettmer <58636741+RADettmer@users.noreply.github.com>
2020-06-08 13:24:27 -0400

    Update logic file

--- codes/logic.js
+++ codes/logic.js
@@ -1,11 +1,4 @@
-// logic.js - Project 2 - Test File
-
-// Create the map with our layers - center on Columbus Ohio
-var map = L.map("map-id", {
-    center: [39.9612, -82.9988],
-    zoom: 11
-    //layers: [streetmap, ]
-});
+// logic.js - Project 2 - Test File - logic
 
 // Create the tile layer that will be the background of our map
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -13,58 +6,144 @@ attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStr
 maxZoom: 18,
 id: "streets-v11",
 accessToken: API_KEY
-}).addTo(map);
+});
 
-// create icon class
-var CarIcon = L.Icon.extend({  
-    options: {
+// Initialize all of the Layer Groups as an object
+var layers = {
+	SEVERITY1: new L.LayerGroup(),
+	SEVERITY2: new L.LayerGroup(),
+	SEVERITY3: new L.LayerGroup(),
+	SEVERITY4: new L.LayerGroup()
+	};
+
+// Create the map with our layers
+// Create the map with our layers - center on Columbus Ohio
+var map = L.map("map-id", {
+    center: [39.9612, -82.9988],
+    zoom: 11,
+    layers: [
+		layers.SEVERITY1,
+		layers.SEVERITY2,
+		layers.SEVERITY3,
+		layers.SEVERITY4
+		]
+});
+
+// add streets map to tile layer
+streetmap.addTo(map);
+
+// Create an overlays
+var overlays = {
+	"Severity 1": layers.SEVERITY1,
+	"Severity 2": layers.SEVERITY2,
+	"Severity 3": layers.SEVERITY3,
+	"Severity 4": layers.SEVERITY4
+	};
+
+// create control layer
+L.control.layers(null, overlays).addTo(map);
+
+// create a legend
+var info = L.control({
+	position: "bottomright"
+	});
+
+// when the layer control is added insert a div with a class of "legend"
+info.onAdd = function() {
+	var div = L.DomUtil.create("div", "legend");
+	return div;
+};
+    
+// add the info legend to the map
+info.addTo(map);
+
+
+// initialze object containing icons
+var icons = {
+    SEVERITY1: new L.Icon({
+        iconUrl: "caraccident_black.png",
         iconSize:     [32, 37],
         iconAnchor:   [16, 37],
         popupAnchor:  [0, -35]
-    }
-});
-
-// create all four icons from this class
-var blackIcon = new CarIcon({iconUrl: "caraccident_black.png"}),
-    redIcon = new CarIcon({iconUrl: "caraccident_red.png"}),
-    orangeIcon = new CarIcon({iconUrl: "caraccident_orange.png"}),
-    yellowIcon = new CarIcon({iconUrl: "caraccident_yellow.png"});
-
-// push class options into icons
-L.icon = function (options) {
-    return new L.Icon(options);
+    }),
+    SEVERITY2: new L.Icon({
+        iconUrl: "caraccident_yellow.png",
+        iconSize:     [32, 37],
+        iconAnchor:   [16, 37],
+        popupAnchor:  [0, -35]
+    }),
+    SEVERITY3: new L.Icon({
+        iconUrl: "caraccident_orange.png",
+        iconSize:     [32, 37],
+        iconAnchor:   [16, 37],
+        popupAnchor:  [0, -35]
+    }),
+    SEVERITY4: new L.Icon({
+        iconUrl: "caraccident_red.png",
+        iconSize:     [32, 37],
+        iconAnchor:   [16, 37],
+        popupAnchor:  [0, -35]
+    })
 };
 
-/*
-/// test icons to see if they appear on map - yes this works!
-L.marker([39.75, -84.230507], {icon:blackIcon}).addTo(map);
-L.marker([39.770382, -84.194901], {icon:orangeIcon}).addTo(map);
-L.marker([39.778061, -84.172005], {icon:redIcon}).addTo(map);
-L.marker([39.79076,	-84.241547], {icon:yellowIcon}).addTo(map);
-*/
 
-//load csv data
-d3.csv("static/data/OH_accident.csv", function(data) {
-    data.forEach(function (landmark) {
-        addMarker(landmark);
-        });
+var accidentStatusCode = [];
+var accidentCount = {
+    SEVERITY1: 0,
+    SEVERITY2: 0,
+    SEVERITY3: 0,
+    SEVERITY4: 0  
+};
+
+
+//load csv data - 
+d3.json("static/data/OH_test.json", function(data) {
+        var Adata = data;  
+        //console.log(Adata);
+        //console.log(Adata.length);
+        Adata.forEach(function(data) {
+               
+                data.Start_Lat = +data.Start_Lat;
+                data.Start_Lng = +data.Start_Lng;
+                //console.log(`data.Start_Lng: ${data.Start_Lng}`);
+                //console.log(`Adata.Start_Lng: ${data.Start_Lng}`);
+        
+                if (data.Severity == 1) {
+                    accidentStatusCode = "SEVERITY1";
+                } else if (data.Severity == 2) {
+                    accidentStatusCode = "SEVERITY2";
+                } else if (data.Severity == 3) {
+                    accidentStatusCode = "SEVERITY3";
+                } else {
+                    accidentStatusCode = "SEVERITY4";
+                }
+                // update accidentStatusCode
+                accidentCount[accidentStatusCode]++;
+                console.log(`accidentStatusCode: ${accidentStatusCode}`)
+                // create new marker??? - this doesn't work
+                var newMarker = L.marker([data.Start_Lat, data.Start_Lng], {
+                    icon: icons[accidentStatusCode]
+                });
+                // add the new marker
+                newMarker.addTo(layers[accidentStatusCode]);
+                // bind popup
+                newMarker.bindPopup(data.ID + "<br> Date: " + data.Start_Time + "<br>" + data.Weather_Condition);
+            //}
+            // update legend
+            updateLegend(accidentCount);
+       }); // end of for each loop	
 });
 
-// plot accidents classifed by serverity 1 to 4 black, yellow, orange, red
-function addMarker(landmark) {
-        if (landmark.Severity === '1') {
-            return L.marker([landmark.Start_Lat, landmark.Start_Lng], {
-                icon: blackIcon}).bindPopup(landmark.ID).addTo(map);
-        } else if (landmark.Severity === '2') {
-            return L.marker([landmark.Start_Lat, landmark.Start_Lng], {
-                icon: yellowIcon}).bindPopup(landmark.ID).addTo(map);
-        } else if (landmark.Severity === '3') {
-            return L.marker([landmark.Start_Lat, landmark.Start_Lng], {
-                icon: orangeIcon}).bindPopup(landmark.ID).addTo(map);
-        } else {
-            return L.marker([landmark.Start_Lat, landmark.Start_Lng], {
-                icon: redIcon}).bindPopup(landmark.ID).addTo(map);
-        }
-    };
+
+// update legend html
+function updateLegend(accidentCount) {
+	document.querySelector(".legend").innerHTML = [
+	"<p class='SEVERITY1'>Severity 1: " + accidentCount.SEVERITY1 + "</p>",
+	"<p class='Severity2'>Severity 2: " + accidentCount.SEVERITY2 + "</p>",
+	"<p class='Severity3'>Severity 3: " + accidentCount.SEVERITY3 + "</p>",
+	"<p class='Severity4'>Severity 4: " + accidentCount.SEVERITY4 + "</p>"	
+	].join("");
+}
+
 
 //end of file
