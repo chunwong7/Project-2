07d5e85516e006cc0319ed29f62854960bd34b51
Randy Dettmer <58636741+RADettmer@users.noreply.github.com>
2020-06-14 14:50:48 -0400

    Update JS

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoicmFkZXR0bWVyIiwiYSI6ImNrYXJqcTBjZzBrOXUyeG52c3A0ejlqMmgifQ.DkoGm785FvAfhTnGsmK8lg";
+const API_KEY = "pk.eyJ1IjoiZWxueTk5IiwiYSI6ImNrYWhjaDFyNzBmYnEyeW11bGJhNXlycXQifQ.d3k8H7jUIodwflc624zzCg";
